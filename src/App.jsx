1c7cd3b2347e411f2882a9a6b004837afa115687
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2024-12-07 00:06:48 +0100

    <feature> add scrip vercel speed-insights

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Layout from "./components/layout/Layout";
 import Home from "./components/pages/Home/Home";
 import Reservations from "./components/pages/Reservations/Reservations";
 import ConfirmedReservation from "./components/pages/Reservations/confirmedReservation";
+import { SpeedInsights } from '@vercel/speed-insights/react';
 
 const App = () => {
   return (
@@ -18,6 +19,7 @@ const App = () => {
           />
         </Routes>
       </Layout>
+      <SpeedInsights />
     </>
   );
 };
