c76d992352c4f9ebdc21e63dd7a89dd7e2b48cd9
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2024-12-09 18:44:19 +0100

    <feat> add link to site img hover

--- src/components/pages/Home/MealCard.jsx
+++ src/components/pages/Home/MealCard.jsx
@@ -1,3 +1,4 @@
+import PropTypes from "prop-types";
 import { faLink } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons";
@@ -7,15 +8,30 @@ const MealCard = ({ meal }) => {
   return (
     <article className="meal-card">
       <div className="meal-card-image">
-        <img src={meal.image} alt={meal.name} />
+        <img src={meal.image} alt={meal.name} loading="lazy" />
+        <div className="meal-card-image-overlay">
+          <a type="button" href={meal.url} target="_blank" rel="noopener noreferrer" className="meal-card-image-overlay-link">
+            Visit the site
+          </a>
+        </div>
       </div>
       <div className="meal-card-header">
         <h3>{meal.name}</h3>
         <div className="meal-card-icons">
-          <a href={meal.github} target="_blank" rel="noopener noreferrer" className="meal-card-github-icon">
-          <FontAwesomeIcon icon={faGithub} />
+          <a
+            href={meal.github}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="meal-card-github-icon"
+          >
+            <FontAwesomeIcon icon={faGithub} />
           </a>
-          <a href={meal.url} target="_blank" rel="noopener noreferrer" className="meal-card-link-icon">
+          <a
+            href={meal.url}
+            target="_blank"
+            rel="noopener noreferrer"
+            className="meal-card-link-icon"
+          >
             <FontAwesomeIcon icon={faLink} />
           </a>
         </div>
@@ -32,6 +48,17 @@ const MealCard = ({ meal }) => {
       </div>
     </article>
   );
+};
+
+MealCard.propTypes = {
+  meal: PropTypes.shape({
+    image: PropTypes.string.isRequired,
+    name: PropTypes.string.isRequired,
+    github: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    description: PropTypes.string.isRequired,
+    badges: PropTypes.arrayOf(PropTypes.string).isRequired,
+  }).isRequired,
 };
 
 export default MealCard;
