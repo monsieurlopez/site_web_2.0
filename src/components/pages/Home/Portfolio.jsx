ca9f1b55c49311dfd7c1a6f08535282ebc61301f
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-02-28 11:31:44 +0100

    <feat> add reack-slick and change style

--- src/components/pages/Home/Portfolio.jsx
+++ src/components/pages/Home/Portfolio.jsx
@@ -1,9 +1,13 @@
 import { HashLink } from "react-router-hash-link";
+import Slider from "react-slick";
 import populationChartImage from "./assets/img-population-chart.png";
+import barbaraGarciaImage from "./assets/img-barbara.png";
 import littleLemonImage from "./assets/logo_litle_lemon.png";
 import inCodeCoin from "./assets/Logo-incodecoin.png";
 import "./Portfolio.css";
 import Cards from "./Cards";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 
 const proyects = [
   {
@@ -29,8 +33,7 @@ const proyects = [
     image: inCodeCoin,
     url: "https://in-code-coin.github.io/in-code-solutions/",
     github: "https://github.com/IN-CODE-COIN/in-code-solutions",
-    description: `
-      The users can access updated information on cryptocurrencies and fiat currencies. The platform provides real-time prices, relevant news, and useful tools such as a currency converter.`,
+    description: `The users can access updated information on cryptocurrencies and fiat currencies. The platform provides real-time prices, relevant news, and useful tools such as a currency converter.`,
     badges: ["HTML", "CSS", "Vanilla Javascript", "Bootstrap", "GridJs", "APIs"],
   },
   {
@@ -49,6 +52,21 @@ const proyects = [
       "Bootstrap",
     ],
   },
+  {
+    name: "Bárbara García Torres",
+    image: barbaraGarciaImage,
+    url: "https://barbara-garcia-torres.vercel.app/",
+    github: "https://github.com/monsieurlopez/barbara-garcia-torres",
+    description: `The website provides clear and accessible information on physiotherapy and lactation consulting services.`,
+    badges: [
+      "HTML",
+      "CSS",
+      "Vanilla Javascript",
+      "APIs",
+      "Bootstrap",
+      "EmailJS"
+    ],
+  },
 ];
 
 const Portfolio = () => {
@@ -57,21 +75,44 @@ const Portfolio = () => {
     window.open(imageUrl, "_blank", "noopener,noreferrer");
   };
 
+  // Configuración del carrusel
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 3, // Mostrar 3 tarjetas por slide
+    slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 1024,
+        settings: {
+          slidesToShow: 2, // 2 tarjetas en tablets
+        },
+      },
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1, // 1 tarjeta en móviles
+        },
+      },
+    ],
+  };
+
   return (
-    <section className="container grid portfolio-projects" id="portfolio">
+    <section className="container portfolio-projects" id="portfolio">
       <div className="portfolio-projects-header">
         <h2>Portfolio</h2>
-        <HashLink
-          className="button-primary"
-          to="/#portfolio"
-          onClick={handleClick}
-        >
+        <HashLink className="button-primary" to="/#portfolio" onClick={handleClick}>
           Download CV
         </HashLink>
       </div>
-      {proyects.map((card, index) => (
-        <Cards key={index} card={card} />
-      ))}
+
+      {/* Carrusel de tarjetas */}
+      <Slider {...settings}>
+        {proyects.map((card, index) => (
+          <Cards key={index} card={card} />
+        ))}
+      </Slider>
     </section>
   );
 };
