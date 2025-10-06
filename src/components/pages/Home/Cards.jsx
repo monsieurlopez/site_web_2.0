6e48b9592abdd58650c520c6e4e8031c1b3199a3
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-10-06 19:45:07 +0200

    <FEAT> update FC Card.jsx

--- src/components/pages/Home/Cards.jsx
+++ src/components/pages/Home/Cards.jsx
@@ -27,7 +27,7 @@ const Project = ({ card }) => {
         <h3>{card.name}</h3>
         <div className="project-card-icons">
           <a
-            href={card.github ? undefined : card.githubPrive}
+            href={card.githubPrive ? undefined : card.github}
             target="_blank"
             rel="noopener noreferrer"
             className={`project-card-github-icon ${card.githubPrive ? "disabled" : ""}`}
@@ -37,12 +37,12 @@ const Project = ({ card }) => {
             <FontAwesomeIcon icon={faGithub} />
           </a>
           <a
-            href={!card.demo ? undefined : card.url}
+            href={!card.demo ? card : undefined}
             target="_blank"
             rel="noopener noreferrer"
             className={`project-card-link-icon ${!card.demo ? "disabled" : ""}`}
             title={`${!card.demo ? "Site not available" : "Go to site"}`}
-            onClick={(e) => card.demo && e.preventDefault()}
+            onClick={(e) => !card.demo && e.preventDefault()}
           >
             <FontAwesomeIcon icon={faLink} />
           </a>
