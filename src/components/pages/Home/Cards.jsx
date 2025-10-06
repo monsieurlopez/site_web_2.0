4a43b17107f00764d69f13de0578dd39bad536c1
monsieurlopez <126283960+monsieurlopez@users.noreply.github.com>
2025-10-06 19:52:19 +0200

    <FEAT> update card.url

--- src/components/pages/Home/Cards.jsx
+++ src/components/pages/Home/Cards.jsx
@@ -37,7 +37,7 @@ const Project = ({ card }) => {
             <FontAwesomeIcon icon={faGithub} />
           </a>
           <a
-            href={card.demo ? card : undefined}
+            href={card.demo ? card.url : undefined}
             target="_blank"
             rel="noopener noreferrer"
             className={`project-card-link-icon ${!card.demo ? "disabled" : ""}`}
